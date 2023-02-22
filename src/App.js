49c5f471aa60f94f21b6854ed26f32f9ed5210c7
mishraom12345 <mishrahariom525@gmail.com>
2023-02-22 10:44:09 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import SignUP from './Component/SingnUp';
+import SignUP from './Component/Pages/SingnUp';
+import Login from './Component/Pages/Login';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Component/Header/Header';
+import Welcome from './Component/Pages/Welcome';
+import { useContext } from 'react';
+import AuthContext from './Component/store/AuthContext';
+import CompleteProfile from './Component/Pages/CompleteProfile';
 
 function App() {
+  const authCtx = useContext(AuthContext)
+  const isLoggedIn = authCtx.isLoggedIn
   return (
     <div>
-     <SignUP/> 
+    <Header/>
+    <Routes>
+    
+     <Route path='signup' element={<SignUP/>} />
+     {<Route  path='/' element={<Login/>}/>}
+     <Route path='welcome' element={<Welcome/>}/>
+     <Route path='completeprofile' element = {<CompleteProfile/>}/>
+    </Routes>
     </div>
-      
   );
 }
 
