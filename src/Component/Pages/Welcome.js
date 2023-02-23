56fba41b3dd04bce7397bc772aa6028e08ef401d
mishraom12345 <mishrahariom525@gmail.com>
2023-02-23 19:36:05 +0530

    Logout User functionality

--- src/Component/Pages/Welcome.js
+++ src/Component/Pages/Welcome.js
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
 import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import AuthContext from '../store/AuthContext'
 const Welcome = () => {
   const authCtx =  useContext(AuthContext)
   const isLoggedIn = authCtx.isLoggedIn
+  const history = useNavigate()
  
   const Varifyemail = () => {
     let token = localStorage.getItem("token");
@@ -34,11 +35,18 @@ const Welcome = () => {
     alert(err.message);
   })
   }
+
+  const LogoutHandler = ()=>{
+    authCtx.Logout()
+    history('/')
+
+  }
     return (
      <div>
       { <div>
        <h5> Welcome to Expense Tracker</h5>
       <p style={{textAlign:'right'}}>
+      <Button variant="outline-danger" onClick={LogoutHandler}>Log Out</Button>
       <Button variant="outline-primary" onClick={Varifyemail}>varify email</Button>
       Your profile is incomplete 
       {<Link to='/completeprofile'>Complete now</Link>}
