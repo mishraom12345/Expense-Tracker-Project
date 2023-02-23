7eb1e8c62f88ad37262e9ac3fc4e9d85e2889dbb
mishraom12345 <mishrahariom525@gmail.com>
2023-02-23 19:25:53 +0530

    Verify User Email functionality

--- src/Component/Pages/Welcome.js
+++ src/Component/Pages/Welcome.js
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import AuthContext from '../store/AuthContext'
 const Welcome = () => {
+  const authCtx =  useContext(AuthContext)
+  const isLoggedIn = authCtx.isLoggedIn
+ 
+  const Varifyemail = () => {
+    let token = localStorage.getItem("token");
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCbUqvti92AIcJw4dvf-vjFT3Q7XGFgSRg',{
+      method:"POST",
+      body:JSON.stringify({
+        requestType:"VERIFY_EMAIL",
+        idToken:token,
+      }),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    }).then((res)=>{
+      if(res.ok){
+          return res.json();
+      }else{
+          return res.json().then((data)=>{
+              if(data && data.error && data.error.message){
+                  let errMessage = "Authentication Failed, " + data.error.message;
+                  throw new Error(errMessage);
+              }
+          })
+      }
+  }).then((data)=>{
+    console.log(data);
+  }).catch((err)=>{
+    alert(err.message);
+  })
+  }
     return (
-      <div>
-        Welcome to Expense Tracker
+     <div>
+      { <div>
+       <h5> Welcome to Expense Tracker</h5>
       <p style={{textAlign:'right'}}>
+      <Button variant="outline-primary" onClick={Varifyemail}>varify email</Button>
       Your profile is incomplete 
-      <Link to='/completeprofile'>Complete now</Link>
+      {<Link to='/completeprofile'>Complete now</Link>}
       </p>  
       <hr/>
-      </div>
+      </div>}
+     </div>
     )
   }
   
